db56a42ad8191f3f114272fd199fcf5c6ce8bbeb
eJay <ejay_young@163.com>
2018-07-13 18:06:35 +0800

    refactor: jest测试配置修改

---
+++ __test__/Dialog.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-14';
+import Dialog from '../src/';
+
+Enzyme.configure({ adapter: new Adapter() });
+
