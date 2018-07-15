e689b18a16cdc0512ce4e122b647f2f9c6627008
eJayYoung <ejay_young@163.com>
2018-07-16 00:41:17 +0800

    test: add render test

--- __test__/Dialog.test.js
+++ __test__/Dialog.test.js
@@ -1,7 +1,42 @@
 import React from 'react';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-14';
 import Dialog from '../src/';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+class DialogTester extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Dialog
+          {...this.props}
+          title="这是dialog的标题"
+          visible={this.state.visible}
+        >
+          <p>这是一个对话框</p>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+describe('Dialog', () => {
+  it('render correctly', () => {
+    const wrapper = mount(<DialogTester />);
+    expect(wrapper.render()).toMatchSnapshot();
+  });
+});
